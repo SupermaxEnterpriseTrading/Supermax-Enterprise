881f1bf1c2973a095c3220fcd479fa056b71a278
SupermaxEnterpriseTrading <supermax.enterprise.tradingcorp@gmail.com>
2026-07-14 15:40:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Replace API_URL after deploying Code.gs as a Google Apps Script Web App.
 // Leave blank to use browser demo mode (localStorage).
 window.SUPERMAX_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwZwCockSJC58tKIw9olnuqOwZDwO9_7nrgcjyoasGBiJwKEg-0NqNcwGWlxmUTgVRAeg/exec",
-  ADMIN_PIN: "SupermaxHR#2026",
+  API_URL: "https://script.google.com/macros/s/AKfycbxSGvRKh1eJiRVxOlipCv7rFEvrFnO8rM6bLqtUZCiyH6BbyfFnisy5PZBIkwZ0vAFg6w/exec",
+  ADMIN_PIN: "Supermax@Hire2026!",
   DEMO_MODE: false
 };
